1a76846eb5bab5fe07158ad6e7f9d635aae15a8f
Kaylyn R Sung <kaylynsung@gmail.com>
2014-09-19 14:47:16 -0700

    added index to sass repo

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+</head>
+
+<body>
+<h1> Hello World </h1>
+<ol>
+	<li>Test Item 1</li>
+	<li>Test Item 2</li>
+	<li>Test Item 3</li>
+</ol>
+<h3>Good bye world</h3>
+</body>
